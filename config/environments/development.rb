e3851dfa448d41468427bc0697d1eb5b5ec1c7a1
Ubuntu <ubuntu@ip-172-31-0-98.ap-northeast-1.compute.internal>
2021-08-04 12:48:44 +0000

    Add account activation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,6 +33,12 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+  
+  host = 'https://851c5fb3e99e400db29602f0ae499925.vfs.cloud9.ap-northeast-1.amazonaws.com/'  # クラウドIDE
+  # クラウドIDEの場合は以下をお使いください
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+  # localhostで開発している場合は以下をお使いください
+  # config.action_mailer.default_url_options = { host: host, protocol: 'http' }
 
   config.action_mailer.perform_caching = false
 
